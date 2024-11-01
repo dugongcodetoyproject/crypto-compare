39dfca1b19d75c00269737890ba5a4e86ef127fd
dugongcode <dugongcode@gmail.com>
2024-11-01 19:30:13 +0900

    currency

--- src/app/page.js
+++ src/app/page.js
@@ -27,7 +27,7 @@ export default function Home() {
   const [prices, setPrices] = useState([]);
   const [loading, setLoading] = useState(true);
   const [lastUpdate, setLastUpdate] = useState(null);
-  const exchangeRate = 1378.6;
+  const exchangeRate = 1377;
 
   const COINS = [
     { symbol: 'BTC', korName: '비트코인' },
