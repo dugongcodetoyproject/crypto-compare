7515b2dba0b5c694447b7ba6ab8116e0d5f8243d
dugongcode <dugongcode@gmail.com>
2024-11-17 20:25:53 +0900

    banner

--- src/app/page.js
+++ src/app/page.js
@@ -229,10 +229,23 @@ function MobileView({ prices, loading, lastUpdate, exchangeRate, coins }) {
       <div className="max-w-7xl mx-auto">
         <div className="mb-2 flex justify-between items-center">
           <div>
-            <h1 className="text-lg font-bold text-gray-900">실시간 김치프리미엄</h1>
+            {/* <h1 className="text-lg font-bold text-gray-900">실시간 김치프리미엄</h1> */}
+            <h1 className="text-lg font-bold text-gray-900 mb-5">
+  실시간 김치프리미엄
+</h1>
+<a href="https://accounts.binance.com/register?ref=X4CHQFBV" target="_blank" rel="noopener noreferrer">
+  <img 
+    src="/icons/binace-pee2.png" 
+    className="block mx-auto mt-4 mb-5" 
+    style={{ width: '400px', height: '70px' }} 
+    alt="Binance"
+  />
+</a>
+
+
             <p className="text-xs text-gray-500">환율: {exchangeRate ? `${exchangeRate.toFixed(2)}원/USD` : '불러오는 중...'}</p>
           </div>
-          <div className="text-xs text-gray-500">업데이트: {lastUpdate}</div>
+          {/* <div className="text-xs text-gray-500">업데이트: {lastUpdate}</div> */}
         </div>
         <div className="bg-white rounded-lg shadow overflow-hidden">
           <table className="min-w-full text-[10px]">
