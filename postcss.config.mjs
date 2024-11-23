2f482d54d8ff22f1a071afd10558246e8eb9ecb5
dugongcode <dugongcode@gmail.com>
2024-11-23 19:11:37 +0900

    re

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,20 @@
-/** @type {import('postcss-load-config').Config} */
-const config = {
-  plugins: {
-    tailwindcss: {},
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "kimchi-compare.vercel.app",
+          },
+        ],
+        destination: "https://www.kimpcoin.com/:path*",
+        permanent: true,
+      },
+    ];
   },
 };
 
-export default config;
+export default nextConfig;
