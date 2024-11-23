e1703a933e3614b331438b4da4213c74e0484a05
dugongcode <dugongcode@gmail.com>
2024-11-23 19:05:12 +0900

    re

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: "/:path*", // 모든 경로
-        destination: "https://kimpcoin.com/:path*", // 새 도메인
-        permanent: true, // 301 리디렉션
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
